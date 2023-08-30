7e49fee059ea70095fb15ae55abf0c0d6e8921b2
moara <moaramirandass@gmail.com>
2023-08-30 16:53:25 +0200

    added global style and theme

---
+++ src/styles/theme.js
@@ -0,0 +1,13 @@
+export default {
+  COLORS: {
+    GRAY_400: "#1C1B1E",
+    GRAY_300: "#262529",
+    GRAY_200: "#948F99",
+    GRAY_100: "#CAC4CF",
+
+    WHITE: "#F4EDE8",
+
+    PINK_300: "#FF859B",
+    PINK_100: "rgba(255, 133, 155, 0.05)",
+  },
+};
