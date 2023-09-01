93eb5b9ff2114f31f49c8cb09640d63dd98cfbeb
moara <moaramirandass@gmail.com>
2023-09-01 15:43:48 +0200

    Rating component

---
+++ src/components/Rating/index.jsx
@@ -0,0 +1,11 @@
+import { Container } from "./styles";
+import { BiStar, BiSolidStar } from "react-icons/bi";
+
+export function Rating({ rating }) {
+  const stars = [1, 2, 3, 4, 5];
+  return (
+    <Container>
+      {stars.map((star) => (star <= rating ? <BiSolidStar /> : <BiStar />))}
+    </Container>
+  );
+}
---
+++ src/components/Rating/styles.js
@@ -0,0 +1,7 @@
+import { styled } from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  gap: 8px;
+  color: ${({ theme }) => theme.COLORS.PINK_300};
+`;
