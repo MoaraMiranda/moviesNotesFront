b6c92314689f0668510ac18818d3f63a7bac4800
moara <moaramirandass@gmail.com>
2023-09-01 15:41:43 +0200

    Section component update

--- src/components/Section/index.jsx
+++ src/components/Section/index.jsx
@@ -1,11 +1,11 @@
 import { Container } from "./styles";
 
-export function Section({ title, children, icon: Icon }) {
+export function Section({ title, children, }) {
   return (
     <Container>
       <h2>{title}</h2>
-      {Icon && <Icon size={20} />}
-      {children}
+      <div>{children}</div>
+      <button>Delete movie</button>
     </Container>
   );
 }
--- src/components/Section/styles.js
+++ src/components/Section/styles.js
@@ -4,15 +4,26 @@ export const Container = styled.section`
   margin: 24px 0;
   display: flex;
   align-items: center;
+  justify-content: space-between;
 
   > h2 {
-    color: ${({ theme }) => theme.COLORS.WHITE};
+    color: ${({ theme }) => theme.COLORS.GRAY_100};
     font-size: 36px;
     font-weight: 500;
     margin-right: 20px;
   }
-  svg {
+  > svg {
     color: ${({ theme }) => theme.COLORS.PINK_300};
     margin-right: 10px;
+  }
+  > button {
+    color: ${({ theme }) => theme.COLORS.PINK_300};
+    background-color: ${({ theme }) => theme.COLORS.GRAY_300};
+    border:  none;
+    padding:  14px;   
+  }
+
+  > div{
+    flex:1;
   }
 `;
