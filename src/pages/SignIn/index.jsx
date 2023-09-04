2286f23e5c353d2aaa21a282ec2282e78ec4999a
moara <moaramirandass@gmail.com>
2023-09-04 19:02:30 +0200

    SignIn page added

---
+++ src/pages/SignIn/index.jsx
@@ -0,0 +1,25 @@
+import { Container, Form, Background } from "./styles.js";
+import { FiMail, FiLock } from "react-icons/fi";
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button/index.jsx";
+
+export function SignIn() {
+  return (
+    <Container>
+      <Form>
+        <div className="logo">
+          <h1>MovieMemos</h1>
+          <p>Take notes from your favorite movies</p>
+        </div>
+        <h2>Sing in here</h2>
+        <div className="input-wrapper">
+          <Input placeholder="Email" icon={FiMail} type="text" />
+          <Input placeholder="Password" icon={FiLock} type="password" />
+          <Button title="Login" />
+        </div>
+        <a href="/">Create your account</a>
+      </Form>
+      <Background />
+    </Container>
+  );
+}
