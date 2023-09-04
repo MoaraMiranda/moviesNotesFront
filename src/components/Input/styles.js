c63691eb53a2deddec9152f4c3103fd52ccdee69
moara <moaramirandass@gmail.com>
2023-09-04 14:38:42 +0200

    Adding flex 1 to input component

--- src/components/Input/styles.js
+++ src/components/Input/styles.js
@@ -1,12 +1,13 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  width: 630px;
+  /* width: 630px; */
   border-radius: 10px;
   background-color: ${({ theme }) => theme.COLORS.GRAY_300};
   color: ${({ theme }) => theme.COLORS.GRAY_200};
   display: flex;
   align-items: center;
+  flex: 1;
 
   > input {
     width: 100%;
@@ -15,6 +16,10 @@ export const Container = styled.div`
     background-color: transparent;
     color: ${({ theme }) => theme.COLORS.WHITE};
     border: none;
+
+    &::placeholder {
+      color: ${({ theme }) => theme.COLORS.GRAY_200};
+    }
   }
 
   > SVG {
