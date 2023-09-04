8aa0f0ed9bce76dccb99f3c7c06aae1e465b7c12
moara <moaramirandass@gmail.com>
2023-09-04 14:40:16 +0200

    CreateMovie page build

---
+++ src/pages/CreateMovie/index.jsx
@@ -0,0 +1,36 @@
+import { Header } from "../../components/Header";
+import { BackLink } from "../../components/BackLink";
+
+import { Container, Textarea, Fieldset } from "./styles";
+import { BiArrowBack } from "react-icons/bi";
+import { Input } from "../../components/Input";
+import { TagsController } from "../../components/TagsController";
+import { PageTitle } from "../../components/PageTitle";
+
+export function CreateMovie() {
+  return (
+    <Container>
+      <Header />
+      <main>
+        <BackLink icon={BiArrowBack} />
+        <PageTitle title="New Movie" buttonTitle="Save" bgPink />
+        <form>
+          <Fieldset>
+            <div className="input-wrapper">
+              <Input placeholder="Title" />
+              <Input placeholder="Your rating (from 0 to 5)" />
+            </div>
+            <Textarea placeholder="Observations" />
+          </Fieldset>
+          <Fieldset>
+            <legend>Tags</legend>
+            <div className="tags">
+              <TagsController value="Romance" />
+              <TagsController isNew placeholder="New Tag" />
+            </div>
+          </Fieldset>
+        </form>
+      </main>
+    </Container>
+  );
+}
---
+++ src/pages/CreateMovie/styles.js
@@ -0,0 +1,79 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: grid;
+  grid-template-rows: 105px auto;
+  grid-template-areas:
+    "header"
+    "content";
+  background-color: ${({ theme }) => theme.COLORS.GRAY_400};
+
+  .tags {
+    display: flex;
+    justify-content: start;
+    gap: 24px;
+    flex-wrap: wrap;
+    background-color: ${({ theme }) => theme.COLORS.BLACK};
+    padding: 16px;
+    border-radius: 10px;
+  }
+
+  .input-wrapper {
+    flex: 1;
+  }
+
+  > main {
+    grid-area: content;
+    padding: 0 120px;
+    margin-top: 50px;
+    overflow-y: scroll;
+
+    h2 {
+      margin-top: 24px;
+      margin-bottom: 40px;
+      font-size: 36px;
+    }
+
+    button {
+      width: 30%;
+    }
+  }
+`;
+
+export const Textarea = styled.textarea`
+  width: 100%;
+  height: 270px;
+  background-color: ${({ theme }) => theme.COLORS.GRAY_300};
+  color: ${({ theme }) => theme.COLORS.GRAY_100};
+  border: none;
+  resize: none;
+  margin-bottom: 40px;
+  border-radius: 10px;
+  padding: 16px;
+
+  &::placeholder {
+    color: ${({ theme }) => theme.COLORS.GRAY_200};
+  }
+`;
+
+export const Fieldset = styled.fieldset`
+  border: none;
+  display: flex;
+  flex-direction: column;
+  gap: 40px;
+  justify-content: space-between;
+  width: 100%;
+
+  > legend {
+    font-size: 20px;
+    font-weight: 400;
+    margin-bottom: 24px;
+  }
+
+  > div {
+    display: flex;
+    gap: 40px;
+  }
+`;
