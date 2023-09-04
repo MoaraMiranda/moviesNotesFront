dff3adffd9ebd432180b5abfb5eeebf65af7f077
moara <moaramirandass@gmail.com>
2023-09-04 19:16:29 +0200

    SignUp page created

---
+++ src/pages/SignUp/index.jsx
@@ -0,0 +1,28 @@
+import { Container, Form, Background } from "./styles.js";
+import { FiMail, FiLock, FiUser } from "react-icons/fi";
+import { BiArrowBack } from "react-icons/bi";
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
+import { BackLink } from "../../components/BackLink";
+
+export function SignUp() {
+  return (
+    <Container>
+      <Form>
+        <div className="logo">
+          <h1>MovieMemos</h1>
+          <p>Take notes from your favorite movies</p>
+        </div>
+        <h2>Create your account</h2>
+        <div className="input-wrapper">
+          <Input placeholder="Name" icon={FiUser} type="text" />
+          <Input placeholder="Email" icon={FiMail} type="text" />
+          <Input placeholder="Password" icon={FiLock} type="password" />
+          <Button title="Register" />
+        </div>
+        <BackLink title="Back to Login" icon={BiArrowBack} />
+      </Form>
+      <Background />
+    </Container>
+  );
+}
---
+++ src/pages/SignUp/styles.js
@@ -0,0 +1,53 @@
+import styled from "styled-components";
+import backgroundImg from "../../assets/backgroundImg.png";
+
+export const Container = styled.div`
+  height: 100vh;
+  display: flex;
+`;
+
+export const Form = styled.form`
+  padding: 0 136px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 48px;
+
+  > h2 {
+    font-size: 24px;
+    font-weight: 500;
+    color: ${({ theme }) => theme.COLORS.GRAY_100};
+  }
+
+  > a {
+    margin-top: 48px;
+    color: ${({ theme }) => theme.COLORS.PINK_300};
+    text-align: center;
+  }
+
+  .logo {
+    > h1 {
+      font-size: 48px;
+      font-weight: 700;
+      color: ${({ theme }) => theme.COLORS.PINK_300};
+    }
+
+    > p {
+      color: ${({ theme }) => theme.COLORS.GRAY_100};
+    }
+  }
+
+  .input-wrapper {
+    display: grid;
+    gap: 8px;
+    width: 340px;
+  }
+`;
+
+export const Background = styled.div`
+  flex: 1;
+  background: url(${backgroundImg}) no-repeat center center;
+  background-size: cover;
+  opacity: 50%;
+`;
